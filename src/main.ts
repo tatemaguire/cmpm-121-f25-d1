3200995beea47f1decd9cb3de9c05f5cacce6094
Tate Maguire <tatemaguir@gmail.com>
2025-10-09 16:37:59 -0700

    autofeeder upgrade1 (STEP 5)

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,27 @@ document.body.innerHTML = `
   <header style="font-size:100px">Cheez-it Eating Simulator</header>
   <button id="tongue-button">😛</button>
   <div>You ate <span id="counter">0</span> cheez-its</div>
+  <button id="upgrade1">AUTOFEEDER MK.I</button>
 `;
 
-const buttonElement: HTMLElement = document.getElementById("tongue-button")!;
-const counterElement: HTMLElement = document.getElementById("counter")!;
-let cheezitCount = 0;
-// deno-lint-ignore prefer-const
-let autofeederRate = 1; // in chzits per second
+const tongueButton = document.getElementById("tongue-button")!;
+const counterElement = document.getElementById("counter")!;
+const upgrade1 = document.getElementById("upgrade1")!;
 
-buttonElement.addEventListener("click", () => {
+let cheezitCount = 0;
+let autofeederRate = 0; // in chzits per second
+const upgrade1Price = 10;
+const upgrade1Effect = 1;
+
+tongueButton.addEventListener("click", () => {
   increaseCheezitCount(1);
+});
+
+upgrade1.addEventListener("click", () => {
+  if (cheezitCount >= upgrade1Price) {
+    cheezitCount -= upgrade1Price;
+    autofeederRate += upgrade1Effect;
+  }
 });
 
 function increaseCheezitCount(count: number) {
