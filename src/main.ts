bb86452229bf3270a73eda40cf1eb605a8237843
Tate Maguire <tatemaguir@gmail.com>
2025-10-09 19:50:55 -0700

    added multiple upgrades and stat display (STEP 6)

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
-// This is my project
-// This is my comment
-
 import "./style.css";
 
 document.body.innerHTML = `
   <header style="font-size:100px">Cheez-it Eating Simulator</header>
   <button id="tongue-button">😛</button>
   <div>You ate <span id="counter">0</span> cheez-its</div>
+  <div>You are being fed <span id="chzitRate">0</span> cheez-its per second</div>
   <div>
     <button id="upgrade1" class="upgradeButton">AUTOFEEDER MK.I</button>
-    <span> MK.I's: 0</span>
+     MK.I's: <span id="upgrade1count">0</span>
   </div>
   <div>
     <button id="upgrade2" class="upgradeButton">AUTOFEEDER MK.II</button>
-    <span> MK.II's: 0</span>
+     MK.II's: <span id="upgrade2count">0</span>
   </div>
   <div>
     <button id="upgrade3" class="upgradeButton">AUTOFEEDER MK.III</button>
-    <span> MK.III's: 0</span>
+     MK.III's: <span id="upgrade3count">0</span>
   </div>
 `;
 
@@ -26,15 +24,21 @@ const counterElement = document.getElementById("counter")!;
 const upgrade1 = document.getElementById("upgrade1")!;
 const upgrade2 = document.getElementById("upgrade2")!;
 const upgrade3 = document.getElementById("upgrade3")!;
+const up1count = document.getElementById("upgrade1count")!;
+const up2count = document.getElementById("upgrade2count")!;
+const up3count = document.getElementById("upgrade3count")!;
+const rateElement = document.getElementById("chzitRate")!;
 
 let cheezitCount = 0;
-let autofeederRate = 0; // in chzits per second
 const upgrade1Price = 10;
 const upgrade1Effect = 0.1;
+let upgrade1Count = 0;
 const upgrade2Price = 100;
 const upgrade2Effect = 2;
+let upgrade2Count = 0;
 const upgrade3Price = 1000;
 const upgrade3Effect = 50;
+let upgrade3Count = 0;
 
 tongueButton.addEventListener("click", () => {
   increaseCheezitCount(1);
@@ -43,23 +47,33 @@ tongueButton.addEventListener("click", () => {
 upgrade1.addEventListener("click", () => {
   if (cheezitCount >= upgrade1Price) {
     cheezitCount -= upgrade1Price;
-    autofeederRate += upgrade1Effect;
+    upgrade1Count++;
+    up1count.innerText = upgrade1Count.toString();
   }
 });
 
 upgrade2.addEventListener("click", () => {
   if (cheezitCount >= upgrade2Price) {
     cheezitCount -= upgrade2Price;
-    autofeederRate += upgrade2Effect;
+    upgrade2Count++;
+    up2count.innerText = upgrade2Count.toString();
   }
 });
 
 upgrade3.addEventListener("click", () => {
   if (cheezitCount >= upgrade3Price) {
     cheezitCount -= upgrade3Price;
-    autofeederRate += upgrade3Effect;
+    upgrade3Count++;
+    up3count.innerText = upgrade3Count.toString();
   }
 });
+
+function getAutofeederRate() {
+  const autoFeederRate = upgrade1Count * upgrade1Effect +
+    upgrade2Count * upgrade2Effect + upgrade3Count * upgrade3Effect;
+  rateElement.innerText = autoFeederRate.toFixed(2);
+  return autoFeederRate;
+}
 
 function increaseCheezitCount(count: number) {
   cheezitCount += count;
@@ -70,7 +84,7 @@ let lastTimestamp: number;
 function autofeederUpdate(timestamp: number) {
   const dt = timestamp - lastTimestamp;
   lastTimestamp = timestamp;
-  increaseCheezitCount((dt / 1000) * autofeederRate);
+  increaseCheezitCount((dt / 1000) * getAutofeederRate());
 
   requestAnimationFrame(autofeederUpdate);
 }
