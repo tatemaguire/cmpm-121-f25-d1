0340178d9ac3a9f4426e5cec7f326527bd1f306c
Tate Maguire <tatemaguir@gmail.com>
2025-10-09 17:40:41 -0700

    for section assignment
    Co-authored-by: LoiselK <KupoDupo@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,40 @@
+// This is my project
+// This is my comment
+
 import "./style.css";
 
 document.body.innerHTML = `
   <header style="font-size:100px">Cheez-it Eating Simulator</header>
   <button id="tongue-button">😛</button>
   <div>You ate <span id="counter">0</span> cheez-its</div>
-  <button id="upgrade1">AUTOFEEDER MK.I</button>
+  <div>
+    <button id="upgrade1" class="upgradeButton">AUTOFEEDER MK.I</button>
+    <span> MK.I's: 0</span>
+  </div>
+  <div>
+    <button id="upgrade2" class="upgradeButton">AUTOFEEDER MK.II</button>
+    <span> MK.II's: 0</span>
+  </div>
+  <div>
+    <button id="upgrade3" class="upgradeButton">AUTOFEEDER MK.III</button>
+    <span> MK.III's: 0</span>
+  </div>
 `;
 
 const tongueButton = document.getElementById("tongue-button")!;
 const counterElement = document.getElementById("counter")!;
 const upgrade1 = document.getElementById("upgrade1")!;
+const upgrade2 = document.getElementById("upgrade2")!;
+const upgrade3 = document.getElementById("upgrade3")!;
 
 let cheezitCount = 0;
 let autofeederRate = 0; // in chzits per second
 const upgrade1Price = 10;
-const upgrade1Effect = 1;
+const upgrade1Effect = 0.1;
+const upgrade2Price = 100;
+const upgrade2Effect = 2;
+const upgrade3Price = 1000;
+const upgrade3Effect = 50;
 
 tongueButton.addEventListener("click", () => {
   increaseCheezitCount(1);
@@ -24,6 +44,20 @@ upgrade1.addEventListener("click", () => {
   if (cheezitCount >= upgrade1Price) {
     cheezitCount -= upgrade1Price;
     autofeederRate += upgrade1Effect;
+  }
+});
+
+upgrade2.addEventListener("click", () => {
+  if (cheezitCount >= upgrade2Price) {
+    cheezitCount -= upgrade2Price;
+    autofeederRate += upgrade2Effect;
+  }
+});
+
+upgrade3.addEventListener("click", () => {
+  if (cheezitCount >= upgrade3Price) {
+    cheezitCount -= upgrade3Price;
+    autofeederRate += upgrade3Effect;
   }
 });
 
